4ad38486e744f44f66e1e18a5f9c64635d569d6f
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2023-02-18 23:19:26 -0300

    Se corrige funcion por si se modifica arrreglo con excusas

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-var arr1 = [
-    ['Un perro ',           'Mi Abuelita ', 'Una tortuga ',         'Mi cacatúa ',                  'Mi vecino '], 
-    ['se comió ',           'meó ',         'me rompió ',           'se robó ',                     'se llevo '], 
-    ['mi calcetín ',        'mi móvil ',    'mi billetera ',        'mis llaves ',                  'mi trabajo '], 
-    ['al salir de casa.',   'hace poco.',   'cuando no miraba.',    'en la hora de mi almuerzo.',   'cuando estaba rezando.']
-];
+var arreglo = [['Un perro ', 'Mi Abuelita ', 'Una tortuga ', 'Mi cacatúa ', 'Mi vecino '], ['se comió ', 'meó ', 'me rompió ', 'se robó ', 'se llevo '], ['mi calcetín ', 'mi móvil ', 'mi billetera ', 'mis llaves ', 'mi trabajo '], ['al salir de casa.', 'hace poco.', 'cuando no miraba.', 'en la hora de mi almuerzo.', 'cuando estaba rezando.']];
 window.onload = () => {
-    var chiva= " ";
+    var chiva= "";
     for (let i=0; i <=3; i++) {
-        let rand = Math.floor(Math.random()*4);
-        var chiva = chiva + arr1[i][rand];
+        let rand = Math.floor(Math.random()*arreglo[i].length);
+        var chiva = chiva+arreglo[i][rand];
     }
-    let j= Math.floor(Math.random()*100);
+    let j=Math.floor(Math.random()*100);
     if (j==0) {
         var chiva = "Heee... paso queee... Nop, no tengo nada.";
     }
